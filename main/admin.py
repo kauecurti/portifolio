e65fe7e4acb69f3e841054267b0477113f19c76b
kaue.curti <kauecurti@hotmail.com>
2024-01-19 14:21:35 -0300

    first commit

---
+++ main/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+# Register your models here.
+from main.models import AboutMe, Education, EducationResponsibilities , Experience , ExperienceResponsibilities, Publication , Skill, Achievement,Certification
+
+admin.site.register(AboutMe)
+admin.site.register(Education)
+admin.site.register(EducationResponsibilities)
+admin.site.register(Experience)
+admin.site.register(ExperienceResponsibilities)
+admin.site.register(Publication)
+admin.site.register(Skill)
+admin.site.register(Achievement)
+admin.site.register(Certification)
