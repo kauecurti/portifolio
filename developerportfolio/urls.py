3dc1c657237d754e55da77984a43d4dafaab2a03
kaue.curti <kauecurti@hotmail.com>
2024-01-20 09:06:36 -0300

    update media

--- developerportfolio/urls.py
+++ developerportfolio/urls.py
@@ -27,4 +27,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',main.views.home, name = 'main-home'),
     path('job/',jobs.views.home, name = 'job-home'),
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+] 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
