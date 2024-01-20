cc0482972dab663f58878f155c856ec76216efa3
kaue.curti <kauecurti@hotmail.com>
2024-01-19 22:10:59 -0300

    update media

--- developerportfolio/settings.py
+++ developerportfolio/settings.py
@@ -138,7 +138,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
-
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
