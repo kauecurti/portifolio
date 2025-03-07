43ccaedf209159aeba245d14236922b8ed7ad622
kaue.curti <kauecurti@hotmail.com>
2025-03-06 22:56:47 -0300

    update

--- developerportfolio/settings.py
+++ developerportfolio/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'developerportfolio.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -101,7 +101,7 @@ DATABASES = {
         'PORT': '5432'
     }
 }
-
+"""
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
